f3c7c5288190fe9cc4db0d8e2ce57b1bcc25d129
Angela <wang.angela90@gmail.com>
2017-11-26 18:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function theBeatlesPlay (musicians, instruments) {
   var emptyArray = [];
 
@@ -7,6 +8,17 @@ function theBeatlesPlay (musicians, instruments) {
   return emptyArray
 }
 
+=======
+function theBeatlesPlay (musicians, instruments) {
+  var emptyArray = [];
+
+  for (var i = 0; i < musicians.length; i++) { //this loop will start from index 0 and stop at the end of the array
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);// pushing to the empty array -> 1st index of musicians combine with play and 1st index of instruments( this is just a tempate)
+  }
+  return emptyArray
+}
+
+>>>>>>> 826d4bbe6dcaf122102b5b07185d3c4b3899781b
 function johnLennonFacts (facts){
 var i = 0;
 while (i < facts.length){
